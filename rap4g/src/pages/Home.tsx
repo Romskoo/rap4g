8154eceb97bfd0d4d4bbc130d6a255eb0fbbaef0
Romain <romain.leunis@ensiie.fr>
2022-10-31 14:53:47 +0100

    update

--- rap4g/src/pages/Home.tsx
+++ rap4g/src/pages/Home.tsx
@@ -1,6 +1,10 @@
 
 import { useLocation,useNavigate } from 'react-router-dom';
 import React,{ useState, useEffect } from 'react';
+import {Form, Button,Col,Row,Container } from 'react-bootstrap';
+import '../css/Bg.css'
+
+
 
 function Home(){
     interface IJoueur{
@@ -9,22 +13,48 @@ function Home(){
         points:number
     }
 
-    const [joueurs, setJoueurs] = useState<IJoueur[]>([{nom:'',numero:1,points:0}]);
+    let navigate = useNavigate();
+    const [joueurs, setJoueurs] = useState<IJoueur[]>([{nom:'',numero:0,points:0}]);
 
-    const changeNom = (e:string)=>{
-
+    const goChoice = () =>{
+        if(joueurs.length > 1){
+            console.log(joueurs)
+            navigate('/Choix',{
+                state:{
+                    joueurs:joueurs
+                }
+            })
+        }       
     }
     return(
-        <div>
-            {joueurs.map((joueur:IJoueur)=>{
-                return(
-                    <div>
-                        <input type='text' placeholder={'joueur'+joueur.numero} onChange={(e:any)=>{joueur.nom = e}}></input>
-                    </div>
-                );
-            })
+        <div className='page'>
+            <Container >
+                <br/>
+                <Row >
+                    <Col ></Col>
+                    <Col>
+                            {joueurs.map((joueur:IJoueur)=>{
+                                return(
+                                    <div key={joueur.numero.toString()}>
+                                        <Form.Control size="lg" type="text" placeholder={'Joueur '+joueur.numero} onChange={(e:any)=>{
+                                                joueur.nom = e.target.value
+                                                if(joueur.numero === joueurs.length-1){
+                                                    setJoueurs( current => [...current,{nom:'',numero:joueurs.length,points:0}])
+                                                }                           
+                                            }} />
+                                        <br/>
+                                    </div>
+                                );
+                            })
 
-            }
+                            }
+                        <div className="d-grid gap-2">
+                            <Button variant="dark" size="lg" onClick={goChoice}> Commencer </Button>
+                        </div>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
